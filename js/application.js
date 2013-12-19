6c757eff234825b0dde44da320fa15bb58fb728c
Pairing Station West <pairing-west@exception>
2013-12-19 13:53:51 -0700

    Added some CRAZY Effects

--- js/application.js
+++ js/application.js
@@ -5,12 +5,25 @@ function init () {
   var random_background = 1 + Math.floor(Math.random() * 5);
   $('body').css("background","#fff url('img/background/"+random_background+".jpg') no-repeat top left");
   $('body').css("background-size", 'cover');
+
+  var custom_callbacks = new Object();
+  custom_callbacks.interval = function() {
+    if (clock.getTime() < 890) {
+      //alert('test');
+      document.title = 'CHANGE PAIRS';
+      document.title = 'CHANGE PAIRS!';
+      $('body').css("background","#fff url('img/background/hypnotoad.png') no-repeat top left");
+    }
+  }
+
   var clock = $('.clock').FlipClock({
       countdown: true,
-      clockFace: 'MinuteCounter'
+      clockFace: 'MinuteCounter',
+      callbacks: custom_callbacks,
+      autoStart: false
   });
+
   clock.setTime(startTime);
-  clock.start();
 
   $('#play_pause').click( function () {
     if (isRunning) {

7aadd9274dea82924da882e654b7791498627646
Pairing Station West <pairing-west@exception>
2013-12-19 10:45:49 -0700

    Made play/pause toggle

--- js/application.js
+++ js/application.js
@@ -1,3 +1,33 @@
-$('.glyphicon-pause').click( function () { clock.stop(); });
-$('.glyphicon-play').click( function () { clock.start(); });
-$('.glyphicon-repeat').click( function () { clock.stop(); clock.setTime(900); });
+function init () {
+  var startTime = 900;
+  var isRunning = false;
+
+  var random_background = 1 + Math.floor(Math.random() * 5);
+  $('body').css("background","#fff url('img/background/"+random_background+".jpg') no-repeat top left");
+  $('body').css("background-size", 'cover');
+  var clock = $('.clock').FlipClock({
+      countdown: true,
+      clockFace: 'MinuteCounter'
+  });
+  clock.setTime(startTime);
+  clock.start();
+
+  $('#play_pause').click( function () {
+    if (isRunning) {
+      clock.stop();
+      isRunning = false;
+      $('#play_pause span').toggleClass('glyphicon-pause glyphicon-play');
+    }
+    else {
+      clock.start();
+      isRunning = true;
+      $('#play_pause span').toggleClass('glyphicon-play glyphicon-pause');
+    }
+  });
+
+  $('#reset').click( function () { clock.stop(); clock.setTime(900); });
+}
+
+$(document).ready(function(){
+  init();
+});
